01f16b99650ec995d86ad7670de3d462e638d778
Dylan Simon <grvdylans@gmail.com>
2025-12-11 11:38:21 -0700

    Flesh out checklist item some more

--- src/components/Checklist.jsx
+++ src/components/Checklist.jsx
@@ -6,7 +6,15 @@ function Checklist() {
         <div>
             <p>Checklist</p>
             <ul className="w-full px-4">
-                <ChecklistItem text="Moss Grotto 1" />
+                <ChecklistItem
+                    text="Moss Grotto 1"
+                    inGameUrl={
+                        "https://checklist-images-guides.s3.us-east-2.amazonaws.com/flea-guide/in-game-images/01.jpg"
+                    }
+                    mapUrl={
+                        "https://checklist-images-guides.s3.us-east-2.amazonaws.com/flea-guide/map-images/01.jpg"
+                    }
+                />
                 <ChecklistItem text="Moss Grotto 2" />
             </ul>
         </div>
--- src/components/ChecklistItem.jsx
+++ src/components/ChecklistItem.jsx
@@ -1,8 +1,8 @@
 import "../tailwind.css";
 
-function ChecklistItem({ text }) {
+function ChecklistItem({ text, mapUrl }) {
     return (
-        <li className="text-black bg-stone-500 shadow-xs shadow-black  transition-all duration-450 my-2 box-border w-full rounded-md flex px-2 py-1">
+        <li className="text-black bg-stone-500 shadow-xs shadow-black  transition-all duration-450 my-2 box-border w-full rounded-md flex px-2 py-2">
             <input
                 name={"checkbox-" + text}
                 id={"checkbox-" + text}
@@ -10,9 +10,13 @@ function ChecklistItem({ text }) {
             />
             <label
                 for={"checkbox-" + text}
-                className="flex text-center justify-center w-full"
+                className="flex-col text-center justify-center w-full"
             >
-                {text}
+                <p>{text}</p>
+                <div className="justify-center flex mb-2">
+                    {/* <img className="w-1/2 justify-center" src={inGameUrl} alt="" /> */}
+                    <img className="w-6/7 justify-center" src={mapUrl} alt="" />
+                </div>
             </label>
         </li>
     );
