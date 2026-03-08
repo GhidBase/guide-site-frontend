2bf07350cbfb1f936448a3e08c374176f8ef41a0
Dylan Simon <grvdylans@gmail.com>
2026-03-08 03:08:09 -0600

    Complete the coloring of the mobile navbar

--- src/components/navbar/MobileNavbar.jsx
+++ src/components/navbar/MobileNavbar.jsx
@@ -1,5 +1,6 @@
 const isLDG = import.meta.env.VITE_LDG == "True";
 import { Link } from "react-router";
+import discordLogo from "../../assets/icons8-discord-50.png";
 
 export default function Navbar({}) {
     console.log("navbar rendered");
@@ -8,76 +9,71 @@ export default function Navbar({}) {
         <div className="mobile-menu-overlay">
             <div
                 id="mobile-menu-panel"
-                className="fixed inset-4 bg-black z-2 items-stretch flex flex-col justify-start "
+                className="fixed inset-4 z-2 items-stretch flex flex-col justify-start bg-(--surface-background) border-(--outline-brown) border-[2px] "
             >
                 <div
                     id="mobile-menu-header"
-                    className="bg-white w-full text-left border-[2px] px-[10px] py-[12px] flex gap-[8px]"
+                    className=" w-full text-left border-b-[2px] px-[10px] py-[12px] flex gap-[8px] border-(--outline-brown) "
                 >
                     <input
                         type="text"
-                        className="flex w-full px-[10px] py-[12px] border-b-[2px] bg-black"
+                        className="flex w-full px-[10px] py-[12px] border-b-[2px] bg-(--primary) text-white border-(--outline-brown) border-[2px] "
+                        placeholder="Search articles..."
                     />
-                    <button className="px-[10px] py-[12px] border-b-[2px] bg-black border-black">
+                    <button className="px-[10px] py-[12px] bg-(--primary) border-(--outline-brown) border-[2px]  ">
                         Close
                     </button>
                 </div>
-                <div className="mobile-menu-feedback bg-white"></div>
+                <div className="mobile-menu-feedback  "></div>
                 <div
                     id="mobile-menu-categories"
-                    className="bg-gray-500 flex flex-col overflow-y-auto px-[12px] py-[8px] flex-1 "
+                    className=" flex flex-col overflow-y-auto px-[12px] py-[8px] flex-1 "
                 >
-                    <div
-                        id="mobile-menu-category"
-                        className="mb-[8px] bg-black w-full "
-                    >
+                    <div id="mobile-menu-category" className="mb-[8px] w-full ">
                         <button
                             id="mobile-menu-cat-header"
-                            className=" w-full text-left bg-black border-[2px] border-white px-[12px] py-[10px] "
+                            className=" w-full text-left border-[2px] border-(--outline-brown) px-[12px] py-[10px] bg-(--primary) "
                         >
                             test
                         </button>
                         <div
                             id="mobile-menu-links "
-                            className="border-[2px] border-t-0  "
+                            className="border-[2px] border-t-0 border-(--outline-brown) "
                         >
                             <Link
                                 to="mythic-categories"
-                                className="block px-[12px] py-[10px] border-t-[1px] border-red-500 "
+                                className="block px-[12px] py-[10px] border-t-[1px] border-(--outline-brown) text-black "
                             >
                                 test
                             </Link>
                             <Link
                                 to="mythic-categories"
-                                className="block px-[12px] py-[10px] border-t-[1px] border-red-500  "
+                                className="block px-[12px] py-[10px] border-t-[1px] border-(--outline-brown) text-black "
                             >
                                 test
                             </Link>
                         </div>
                     </div>
-                    <div
-                        id="mobile-menu-category"
-                        className="mb-[8px] bg-black w-full "
-                    >
+                    <div id="mobile-menu-category" className="mb-[8px] w-full ">
                         <button
                             id="mobile-menu-cat-header"
-                            className=" w-full text-left bg-black border-[2px] border-white px-[12px] py-[10px] "
+                            className=" w-full text-left border-[2px] border-(--outline-brown) px-[12px] py-[10px] bg-(--primary) "
                         >
                             test
                         </button>
                         <div
                             id="mobile-menu-links "
-                            className="border-[2px] border-t-0  "
+                            className="border-[2px] border-t-0 border-(--outline-brown) "
                         >
                             <Link
                                 to="mythic-categories"
-                                className="block px-[12px] py-[10px] border-t-[1px] border-red-500 "
+                                className="block px-[12px] py-[10px] border-t-[1px] border-(--outline-brown) text-black "
                             >
                                 test
                             </Link>
                             <Link
                                 to="mythic-categories"
-                                className="block px-[12px] py-[10px] border-t-[1px] border-red-500  "
+                                className="block px-[12px] py-[10px] border-t-[1px] border-(--outline-brown) text-black "
                             >
                                 test
                             </Link>
@@ -86,29 +82,32 @@ export default function Navbar({}) {
                 </div>
                 <div
                     id="mobile-menu-persistent"
-                    className="flex flex-col items-center py-3 "
+                    className="flex flex-col items-center pt-4 pb-2 text-black px-3 border-t-[2px] "
                 >
-                    <a href="https://discord.com/invite/luckydefense">
-                        Discord
+                    <a
+                        href="https://discord.com/invite/luckydefense"
+                        className="flex items-center justify-center gap-2 bg-[#5865f2] py-2 px-4 rounded-md "
+                    >
+                        <img src={discordLogo} className="h-[1.25em] " />
+                        <p className="text-white text-[0.8em] ">
+                            Join us on Discord
+                        </p>
                     </a>
-                    <div className="px-8 w-full my-3 ">
-                        <hr className="w-full border-t border-white/50  " />
+                    <div className="px-8 w-full my-2 ">
+                        <hr className="w-full border-t border-black/50  " />
                     </div>
-                    <div id="nav-footer" className="text-[0.8em] text-center ">
+                    <div id="nav-footer" className="text-[0.6em] text-center ">
                         © 2025 LuckyDefenseGuides.com. This site is not
                         affiliated with or endorsed by the creators of Lucky
-                        Defense.
+                        Defense. |{" "}
+                        <a
+                            id="privacy-policy"
+                            className=""
+                            href="/pages/privacy-policy.html"
+                        >
+                            Privacy Policy
+                        </a>
                     </div>
-                    <div className="px-8 w-full my-3  ">
-                        <hr className="w-full border-t border-white/50 " />
-                    </div>
-                    <a
-                        id="privacy-policy"
-                        className="text-[0.8em] "
-                        href="/pages/privacy-policy.html"
-                    >
-                        Privacy Policy
-                    </a>
                 </div>
             </div>
         </div>
