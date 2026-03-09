0c2b93b7fb7d5f98f6bf62ad279a7f00caf4b6c1
Dylan Simon <grvdylans@gmail.com>
2026-03-09 01:29:18 -0600

    Add and animate collapsing - and general fixes

--- src/components/navbar/MobileNavbar.jsx
+++ src/components/navbar/MobileNavbar.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 const isLDG = import.meta.env.VITE_LDG == "True";
 import discordLogo from "../../assets/icons8-discord-50.png";
 import MobileNavbarCategory from "./MobileNavbarCategory";
@@ -7,10 +8,11 @@ export default function MobileNavbar({ toggleNav, navOpen }) {
     console.log("navbar rendered");
     const { sectionsMap } = useLoaderData();
     const sections = Array.from(sectionsMap.values());
+    const [openedSection, setOpenedSection] = useState(sections[0].id);
 
     return (
         <div
-            className={`mobile-menu-overlay bg-black/40 w-full h-full fixed inset z-2   ${!navOpen ? "hidden" : " "} `}
+            className={`mobile-menu-overlay bg-black/40 w-full h-full fixed inset z-2   ${!navOpen ? "hidden" : " "} lg:hidden text-[0.8em]`}
             onClick={(e) => {
                 if (e.target === e.currentTarget) {
                     toggleNav();
@@ -19,19 +21,19 @@ export default function MobileNavbar({ toggleNav, navOpen }) {
         >
             <div
                 id="mobile-menu-panel"
-                className="rounded-lg fixed inset-4 top-16 bottom-16 z-2 items-stretch flex flex-col justify-start bg-(--surface-background) border-(--outline-brown) border-[2px] shadow-lg shadow-black/50 "
+                className="rounded-lg fixed inset-4 top-16 bottom-10 z-2 items-stretch flex flex-col justify-start bg-(--surface-background) border-(--outline-brown) border-[2px] shadow-lg shadow-black/50 "
             >
                 <div
                     id="mobile-menu-header"
-                    className=" w-full text-left border-b-[2px] px-[10px] py-[12px] flex gap-[8px] border-(--outline-brown) "
+                    className=" w-full text-left border-b-[2px] px-[12px] py-[12px] flex gap-[8px] border-(--outline-brown) "
                 >
                     <input
                         type="text"
-                        className="flex w-full px-[10px] py-[12px] border-b-[2px] bg-(--primary) text-white border-(--outline-brown) border-[2px] "
+                        className="flex w-full px-[12px] py-[12px] border-b-[2px] bg-(--primary) text-white border-(--outline-brown) border-[2px] "
                         placeholder="Search articles..."
                     />
                     <button
-                        className="px-[10px] py-[12px] bg-(--primary) border-(--outline-brown) border-[2px] "
+                        className="px-[12px] py-[12px] bg-(--primary) border-(--outline-brown) border-[2px] "
                         onClick={toggleNav}
                     >
                         Close
@@ -48,6 +50,8 @@ export default function MobileNavbar({ toggleNav, navOpen }) {
                                 key={section.id}
                                 toggleNav={toggleNav}
                                 section={section}
+                                openedSection={openedSection}
+                                setOpenedSection={setOpenedSection}
                             />
                         );
                     })}
@@ -57,7 +61,7 @@ export default function MobileNavbar({ toggleNav, navOpen }) {
                     className="flex flex-col items-center pt-2.5 pb-2 text-black px-3 border-t-[2px]  "
                 >
                     <div className="flex items-center gap-2 ">
-                        <p className=" text-[0.8em] ">Join the community: </p>
+                        <p className="  ">Join the community: </p>
                         <a
                             href="https://discord.com/invite/luckydefense"
                             className="flex items-center justify-center gap-2 bg-[#5865f2] py-2 px-4 rounded-md "
@@ -69,7 +73,7 @@ export default function MobileNavbar({ toggleNav, navOpen }) {
                     <div className="px-8 w-full my-2 ">
                         <hr className="w-full border-t border-black/50  " />
                     </div>
-                    <div id="nav-footer" className="text-[0.6em] text-center ">
+                    <div id="nav-footer" className=" text-[0.8em] text-center ">
                         © 2025 LuckyDefenseGuides.com. This site is not
                         affiliated with or endorsed by the creators of Lucky
                         Defense. |{" "}
--- src/components/navbar/MobileNavbarCategory.jsx
+++ src/components/navbar/MobileNavbarCategory.jsx
@@ -1,20 +1,39 @@
 import { Link } from "react-router";
+import { useRef } from "react";
 
-export default function MobileNavbarCategory({ section, toggleNav }) {
+export default function MobileNavbarCategory({
+    section,
+    toggleNav,
+    openedSection,
+    setOpenedSection,
+}) {
     const title = section.title;
     const pages = section.pages;
+    const linksRef = useRef(null);
     const isLDG = import.meta.env.VITE_LDG == "True";
     return (
         <div id="mobile-menu-category" className="mb-[8px] w-full ">
             <button
                 id="mobile-menu-cat-header"
                 className=" w-full text-left border-[2px] border-(--outline-brown) px-[12px] py-[10px] bg-(--primary) "
+                onClick={() => {
+                    console.log(linksRef.current.scrollHeight);
+                    setOpenedSection(section.id);
+                }}
             >
-                {title}
+                {title} ({pages.length})
             </button>
+
             <div
-                id="mobile-menu-links "
-                className="border-[2px] border-t-0 border-(--outline-brown) "
+                ref={linksRef}
+                id="mobile-menu-links"
+                className="border-[2px] border-t-0 border-(--outline-brown) overflow-hidden transition-all duration-300 ease-in-out"
+                style={{
+                    height:
+                        openedSection === section.id
+                            ? linksRef.current?.scrollHeight + "px"
+                            : "0px",
+                }}
             >
                 {pages.map((page) => {
                     var actualSlug;
@@ -27,6 +46,7 @@ export default function MobileNavbarCategory({ section, toggleNav }) {
                     console.log(actualSlug);
                     return (
                         <Link
+                            key={page.id}
                             className="block px-[12px] py-[10px] border-t-[1px] border-(--outline-brown) text-black "
                             to={page.slug}
                             onClick={toggleNav}
