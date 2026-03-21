e3d6f084369b3bb5f281aa1a38d27aeaab6750a5
Dylan Simon <grvdylans@gmail.com>
2026-03-20 19:31:03 -0600

    Remove LDG only ads.txt restriction

--- app/routes/ads-txt.jsx
+++ app/routes/ads-txt.jsx
@@ -1,8 +1,4 @@
-export async function loader({ request }) {
-    const hostname = new URL(request.url).hostname;
-    if (!hostname.includes("luckydefenseguides")) {
-        throw new Response(null, { status: 404 });
-    }
+export async function loader() {
     return new Response(
         "google.com, pub-7281531232813347, DIRECT, f08c47fec0942fa0",
         { headers: { "Content-Type": "text/plain" } },
