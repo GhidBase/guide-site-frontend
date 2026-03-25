9f8a67f3a0b23876599e390f4c99002a762befb8
Dylan Simon <grvdylans@gmail.com>
2026-03-25 13:59:58 -0600

    Start making an idle game

--- app/routes.js
+++ app/routes.js
@@ -58,6 +58,7 @@ export default [
         route("board-builder-admin", "routes/board-builder.jsx", { id: "top-board-builder" }),
         route("tier-list", "routes/tier-list.jsx", { id: "top-tier-list" }),
 
+        route("idle", "routes/idle-game.jsx"),
         route("login", "routes/login.jsx"),
         route("signup", "routes/signup.jsx"),
         route("dashboard", "routes/dashboard.jsx"),
